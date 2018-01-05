563a8146a965d8c14c619f46e43808f02de18c80
btcline04 <btcline04@gmail.com>
2018-01-04 23:03:23 -0500

    Add show page to show a created golf bag

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -1,9 +1,23 @@
 class BagsController < ApplicationController
+  before_action :find_bag, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
   def new
     @bag = Bag.new
+  end
+
+  def create
+    @bag = Bag.new(bag_params)
+    if @bag.save
+      redirect_to bag_path(@bag)
+    else
+      redirect_to new_bag_path, notice: "Try again!"
+    end
+  end
+
+  def show
   end
 
   def edit
