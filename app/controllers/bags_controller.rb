e5f3b136f747c9ac13213b8019496bdbde7d42b4
btcline04 <btcline04@gmail.com>
2018-03-13 17:03:24 -0400

    Add simple_form to Bags form for easier readability

--- app/controllers/bags_controller.rb
+++ app/controllers/bags_controller.rb
@@ -13,7 +13,6 @@ class BagsController < ApplicationController
 
   def create
     @bag = current_user.bags.build(bag_params)
-    binding.pry
     if @bag.save
       redirect_to bag_path(@bag)
     else
