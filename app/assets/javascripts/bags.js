24ee9c17042cade2f02336e21e0f938905144b88
btcline04 <btcline04@gmail.com>
2018-06-14 22:54:20 -0400

    Create bags.js and refactor code there to simplify the bags index view

---
+++ app/assets/javascripts/bags.js
@@ -0,0 +1,29 @@
+function iterateOverBags() {  
+  $.get("/bags/bag_data", function(data, status) {
+    data.forEach(bag => {
+      let newBag = new Bag(bag)
+      console.log(newBag);
+      let postHtml = newBag.formatIndex()
+      $('#bags-data').append(postHtml)
+    })
+  })
+};
+
+
+// Creates Bag constructor function and prototype - each Bag object
+// now has access to the formatIndex() method
+function Bag (brand, title) {
+  this.brand = brand
+  this.title = title
+};
+
+Bag.prototype.formatIndex = function() {
+  let postHtml = `
+    <a href="/bags/${this.brand["id"]}"><p>${this.brand["title"]}</p></a>
+    <p>${this.brand["brand"]}</p>
+    <br>
+  `
+  return postHtml;
+};
+
+
